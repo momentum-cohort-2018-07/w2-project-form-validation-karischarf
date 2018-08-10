9a8270d6a1f2cb42a8f4d9a55d9f5bce09571030
Kari Scharf <karischarf@gmail.com>
2018-08-10 15:29:30 -0400

    name field value turning green/red

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Add validation!');
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+// clearError()
+
+var nameFieldValue = document.getElementById('name').value
+console.log(nameFieldValue)
+
+// check and make sure name is not empty
+if (nameFieldValue === '') {
+    showEmptyNameError()
+} else {
+    var validName = document.getElementById('name-field')
+    validName.classList.add('input-valid')
+}
+})
+
+function showEmptyNameError() {
+    var errorDiv = document.createElement('div')
+    errorDiv.classList.add('input-invalid')
+    errorDiv.innerText = 'cannot be blank'
+
+    var field = document.getElementById('name')
+    field.parentElement.appendChild(errorDiv)
+    field.parentElement.classList.add('input-invalid')
+}
+
